f5d9350c07a58705f40f859045cdd605ae0dd3bf
Thibaud Cournollet <cournoll@adobe.com>
2021-01-15 09:28:34 +0100

    update

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { ExampleComponent } from "web-cloud-frontend-commons/example";
+import "web-cloud-frontend-commons/styles.css";
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />;
+};
+
+export default App;
